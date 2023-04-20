3033588102fce824199cc2e4aa5d3e9e8bf76105
Xzawier <xzawier1777@hotmail.com>
2023-04-20 17:25:21 +0200

    Yolo

--- Udemy_00.cpp
+++ Udemy_00.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main(){
-    
-    int i = 4;
+
+    int i = 4;  // CORRECT by LANGUAGE-WISE and READABLE
     i = i / 2;
     std::cout << "The KB-Result is " << i << std::endl;
 
